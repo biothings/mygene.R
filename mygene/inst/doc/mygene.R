0d8b72b90c67e28169bcaf91c68b7adcd50b040d
Adam Mark <adammark@scripps.edu.>
2014-07-31 12:49:11 -0400

    docs updated

--- mygene/inst/doc/mygene.R
+++ mygene/inst/doc/mygene.R
@@ -14,52 +14,64 @@ source('~/Documents/Su_Lab/Su_Lab-clone/mygene/R/utils.R')
 
 
 ###################################################
-### code chunk number 3: mygene.Rnw:36-37
+### code chunk number 3: mygene.Rnw:36-42
 ###################################################
-getGene("1017", fields=c("name","symbol","taxid","entrezgene"))
+gene=getGene("1017", fields="all")
+length(gene)
+gene$name
+gene$taxid
+gene$uniprot
+gene$refseq
 
 
 ###################################################
-### code chunk number 4: mygene.Rnw:47-48
+### code chunk number 4: mygene.Rnw:52-53
 ###################################################
-getGenes(c("1017","1018","ENSG00000148795"), species="human")
+getGenes(c("1017","1018","ENSG00000148795"))
 
 
 ###################################################
-### code chunk number 5: mygene.Rnw:62-63
+### code chunk number 5: mygene.Rnw:67-68
 ###################################################
 query(q="cdk2", size=5)
 
 
 ###################################################
-### code chunk number 6: mygene.Rnw:73-74
+### code chunk number 6: mygene.Rnw:71-72
 ###################################################
-queryMany(c(1017, 695), scopes="entrezgene", species="human")
+query(q="NM_013993")
 
 
 ###################################################
-### code chunk number 7: mygene.Rnw:89-99
+### code chunk number 7: mygene.Rnw:82-84
 ###################################################
-xli<-c('DDX26B',
- 'CCDC83',
- 'MAST3',
- 'FLOT1',
- 'RPL11',
- 'ZDHHC20',
- 'LUC7L3',
- 'SNORD49A',
- 'CTSH',
- 'ACOT8')
+queryMany(c('1053_at', '117_at', '121_at', '1255_g_at', '1294_at'),
+          scopes="reporter", species="human")
 
 
 ###################################################
-### code chunk number 8: mygene.Rnw:104-105
+### code chunk number 8: mygene.Rnw:99-109
+###################################################
+xli<-c('DDX26B', 
+       'CCDC83', 
+       'MAST3', 
+       'FLOT1', 
+       'RPL11', 
+       'ZDHHC20', 
+       'LUC7L3', 
+       'SNORD49A', 
+       'CTSH', 
+       'ACOT8')
+
+
+###################################################
+### code chunk number 9: mygene.Rnw:114-115
 ###################################################
 queryMany(xli, scopes="symbol", fields="entrezgene", species="human")
 
 
 ###################################################
-### code chunk number 9: mygene.Rnw:112-115
+### code chunk number 10: mygene.Rnw:122-125
 ###################################################
 out<-queryMany(xli, scopes="symbol", fields="ensembl.gene", species="human")
 out
@@ -67,42 +79,42 @@ out$ensembl.gene[[4]]
 
 
 ###################################################
-### code chunk number 10: mygene.Rnw:123-130
+### code chunk number 11: mygene.Rnw:133-140
 ###################################################
-xli<-c('DDX26B',
- 'CCDC83',
- 'MAST3',
- 'FLOT1',
- 'RPL11',
- 'Gm10494')
+xli<-c('DDX26B', 
+       'CCDC83', 
+       'MAST3', 
+       'FLOT1', 
+       'RPL11', 
+       'Gm10494')
 queryMany(xli, scopes="symbol", fields="entrezgene", species="human")
 
 
 ###################################################
-### code chunk number 11: mygene.Rnw:135-144
+### code chunk number 12: mygene.Rnw:145-154
 ###################################################
-xli<-c('DDX26B',
- 'CCDC83',
- 'MAST3',
- 'FLOT1',
- 'RPL11',
- 'Gm10494',
- '1007_s_at',
- 'AK125780')
+xli<-c('DDX26B', 
+       'CCDC83', 
+       'MAST3', 
+       'FLOT1', 
+       'RPL11', 
+       'Gm10494', 
+       '1007_s_at', 
+       'AK125780')
 
 
 
 ###################################################
-### code chunk number 12: mygene.Rnw:149-153
+### code chunk number 13: mygene.Rnw:159-163
 ###################################################
-out<-queryMany(xli, scopes=c("symbol","reporter","accession"), 
+out<-queryMany(xli, scopes=c("symbol", "reporter","accession"), 
              fields=c("entrezgene","uniprot"), species="human")
 out
 out$`uniprot.Swiss-Prot`[[5]]
 
 
 ###################################################
-### code chunk number 13: mygene.Rnw:164-166
+### code chunk number 14: mygene.Rnw:174-176
 ###################################################
 queryMany(xli, scopes=c("symbol", "reporter", "accession"), 
           fields=c("entrezgene", "uniprot"), species='human', returnall=TRUE)
